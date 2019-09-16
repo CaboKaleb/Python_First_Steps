030aa70accd56584136f8e1d9abdf40b98f9f8bf
Cabo <calebquesada2@gmail.com>
2019-09-16 16:51:18 -0600

    First Commit

---
+++ Sets.py
@@ -0,0 +1,17 @@
+#A set is like a list but ignoresthe repeated values
+
+Set_Example= {1,2,3,4,4,4}
+
+print(Set_Example)
+
+#Challenge! Create a method where you receive a list with repeated elements and return it back without repeated elements
+
+def clearlist(initial):
+    initial=set(initial)
+    list(initial).sort(reverse=True)
+    return initial
+
+print(clearlist(["a", "a", "a", "a", "b", "b", "c", "c", "c", "c", "d", "d", "d", "e", "e"]))
+
+
+
